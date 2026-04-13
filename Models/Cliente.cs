f26a6bb4f244a5728c0d89122012e3ba436ad1f3
Standy Cerna <standycerna@gmail.com>
2026-04-13 18:39:45 -0500

    Agregue los paquete tools, design, SQL y core al proyecto

---
+++ Models/Cliente.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Proyecto_CPS.Models
+
+{
+    public class Cliente
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdCliente { get; set; }
+
+        [Required, StringLength(50)]
+        public string PrimNombre { get; set; }
+
+        [StringLength(50)]
+        public string SegNombre { get; set; }
+
+        [Required, StringLength(50)]
+        public string ApellidoP { get; set; }
+
+        [Required, StringLength(50)]
+        public string ApellidoM { get; set; }
+
+        [Required, StringLength(20)]
+        public string Telefono { get; set; }
+
+        [Required, StringLength(100)]
+        public string Correo { get; set; }
+
+        // Relaciones
+        public ICollection<Reserva>? Reservas { get; set; }
+        public ICollection<Comentario>? Comentarios { get; set; }
+
+        // Relación opcional con Usuario
+        public int? UsuarioId { get; set; }   // FK opcional
+        public Usuario? Usuario { get; set; } // Navegación opcional
+    }
+}
+
--- obj/Debug/net8.0/Proyecto-CPS.AssemblyInfo.cs
+++ obj/Debug/net8.0/Proyecto-CPS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto-CPS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e3ba1b7a34f485800226b5484cf44c8c89d7c42")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto-CPS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto-CPS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
